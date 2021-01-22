3d7f4b5c38b50d6cf007b91ba5fb092cea175b67
fredrikwide <fredrik.widegren@gmail.com>
2021-01-22 11:57:23 +0100

    friday fun fest 2021

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import { useForm } from 'react-hook-form'
 import './App.css';
 
-interface AppProps { }
 
-function App({ }: AppProps) {
+interface TODO {
+  title: String,
+  completed: Boolean,
+  description: String,
+
+}
+
+
+const App = () => {
+
+  const [addTodo, setAddTodo] = useState<TODO>({
+    title: "",
+    completed: false,
+    description: "",
+
+  })
+  const [todos, setTodos] = useState([])
+  const handleSetTitle = () => {
+
+  }
+
+  const handleSetDesc = () => {
+
+  }
+
+
+  const { register, handleSubmit } = useForm<TODO>(); const onSubmit = (data: TODO) => { console.log("data", data); };
 
   return (
+
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <section className="wrapper">
+        <div className="todos-container">
+          <h1>typescript Todos</h1>
+          <div className="todo">
+            <h2>{addTodo.title}</h2>
+            <ul className="list">
+              <li className="listitem">1</li>
+              <li className="listitem">2</li>
+              <li className="listitem">3</li>
+            </ul>
+          </div>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-container">
+            <div className="field">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                ref={register}
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="description">Description</label>
+              <textarea
+
+                id="description"
+                name="description"
+                ref={register}
+              />
+            </div>
+            <div className="button-container">
+              <button className="submit" type="submit">Save</button>
+            </div>
+          </div>
+        </form>
+      </section>
     </div>
   );
 }
